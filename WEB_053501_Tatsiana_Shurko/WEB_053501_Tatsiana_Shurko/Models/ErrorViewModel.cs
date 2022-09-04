97455e337b0315e7fd7095e6ea0c8e7246115f72
Tanusha <tanusha20020@tut.by>
2022-09-05 00:55:55 +0300

    Add Lab1

---
+++ WEB_053501_Tatsiana_Shurko/WEB_053501_Tatsiana_Shurko/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_053501_Tatsiana_Shurko.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
