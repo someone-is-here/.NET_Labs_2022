e0e0e1a4f96c074d4bd13b3b162b206889e3cd35
Tanusha <tanusha20020@tut.by>
2022-10-28 00:45:29 +0300

    Add lab08

---
+++ WEB_053501_Tatsiana_Shurko/Blazor1/Client/Counter.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor1.Client{
+    public class CounterModel {
+        [Required]
+        [Range(0, Int32.MaxValue, ErrorMessage = $"Number must be between 0 to 2147483647")]
+        public int Number { get; set; }
+
+        public CounterModel() {
+            Number = 0;
+        }
+    }
+}
---
+++ WEB_053501_Tatsiana_Shurko/Blazor1/Server/Controllers/BooksController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazor1.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEB_053501_Tatsiana_Shurko.Data;
+using WEB_053501_Tatsiana_Shurko.Models;
+
+namespace Blazor1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase {
+        private readonly ILogger _logger;
+        private readonly BookContext _context;
+
+        public BooksController(BookContext context, ILogger<BooksController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public async Task<ActionResult<ListViewModel>> GetBooks()
+        {
+            List<Book> books = await _context.Books.ToListAsync();
+
+            ListViewModel list = new();
+
+            list.listWithBooks = new();
+
+            foreach (var item in books) {
+                list.listWithBooks.Add(new DetailViewModel {
+                    Id = item.Id,
+                    CategoryId = item.CategoryId,
+                    Title = item.Title,
+                    Discription = item.Discription,
+                    Price = item.Price,
+                    ImagePath = item.ImagePath,
+                    MimeType = item.MimeType,
+                });
+            }
+
+            return list;
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        // PUT: api/Books/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Books
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+        }
+
+        // DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.Id == id);
+        }
+    }
+}
